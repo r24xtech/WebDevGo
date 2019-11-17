4fa20b13f1c2d2428f155e9e181be40b5e7c22e0
r24xtech <57235265+r24xtech@users.noreply.github.com>
2019-11-17 14:32:19 -0300

    Add files via upload

---
+++ handlers.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+  "errors"
+  "fmt"
+  "net/http"
+  "strconv"
+  "r24xtech.net/snippetbox/pkg/models"
+)
+
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+  if r.URL.Path != "/" {
+    app.notFound(w)
+    return
+  }
+  s, err := app.snippets.Latest()
+  if err != nil {
+    app.serverError(w, err)
+    return
+  }
+
+  app.render(w, r, "home.page.tmpl", &templateData {
+    Snippets: s,
+  })
+}
+
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
+  id, err := strconv.Atoi(r.URL.Query().Get("id"))
+  if err != nil || id < 1 {
+    app.notFound(w)
+    return
+  }
+  s, err := app.snippets.Get(id)
+  if err != nil {
+    if errors.Is(err, models.ErrNoRecord) {
+      app.notFound(w)
+    } else {
+      app.serverError(w, err)
+    }
+    return
+  }
+
+  app.render(w, r, "show.page.tmpl", &templateData {
+    Snippet: s,
+  })
+}
+
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
+  if r.Method != http.MethodPost {
+    w.Header().Set("Allow", http.MethodPost)
+    app.clientError(w, http.StatusMethodNotAllowed)
+    return
+  }
+  title := "O snial"
+  content := "O snial\nClimb Mount Fuji,\nBut slowly, slowly!\n\n- Kobayashi Issa"
+  expires := "7"
+  id, err := app.snippets.Insert(title, content, expires)
+  if err != nil {
+    app.serverError(w, err)
+    return
+  }
+  http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
+}
