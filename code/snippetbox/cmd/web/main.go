95e91bf97f0f846277260652140d6c17a9210871
r24xtech <57235265+r24xtech@users.noreply.github.com>
2019-11-19 15:06:35 -0300

    Add files via upload

---
+++ code/snippetbox/cmd/web/main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+  "crypto/tls"
+  "database/sql"
+  "flag"
+  "html/template"
+  "log"
+  "net/http"
+  "os"
+  "time"
+  "strings"
+  "r24xtech.net/snippetbox/pkg/models/mysql"
+  _ "github.com/go-sql-driver/mysql"
+  "github.com/golangcollege/sessions"
+)
+
+type contextKey string
+
+const contextKeyIsAuthenticated = contextKey("isAuthenticated")
+
+type application struct {
+  errorLog *log.Logger
+  infoLog *log.Logger
+  session *sessions.Session
+  snippets *mysql.SnippetModel
+  templateCache map[string]*template.Template
+  users *mysql.UserModel
+}
+
+func main() {
+  addr := flag.String("addr", ":4000", "HTTP network address")
+  dsn := flag.String("dsn", "web:9jaZxapeoy4n1qk2w@/snippetbox?parseTime=true", "MySQL data source name")
+  secret := flag.String("secret", "s6Ndh+pPbnzHbS*+9Pk8qGWhTzbpa@ge", "Secret key")
+  flag.Parse()
+
+  infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+  errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+  db, err := openDB(*dsn)
+  if err != nil {
+    errorLog.Fatal(err)
+  }
+
+  defer db.Close()
+
+  templateCache, err := newTemplateCache("./ui/html")
+  if err != nil {
+    errorLog.Fatal(err)
+  }
+
+  session := sessions.New([]byte(*secret))
+  session.Lifetime = 12 *time.Hour
+  session.Secure = true
+  //session.SameSite = http.SameSiteStrictMode
+
+  app := &application{
+    errorLog: errorLog,
+    infoLog: infoLog,
+    session: session,
+    snippets: &mysql.SnippetModel{DB: db},
+    templateCache: templateCache,
+    users: &mysql.UserModel{DB: db},
+  }
+
+  tlsConfig := &tls.Config{
+    PreferServerCipherSuites: true,
+    CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
+  }
+
+  srv := &http.Server{
+    Addr: *addr,
+    ErrorLog: errorLog,
+    Handler: app.routes(),
+    TLSConfig: tlsConfig,
+    IdleTimeout: time.Minute,
+    ReadTimeout: 5 * time.Second,
+    WriteTimeout: 10 * time.Second,
+  }
+
+  infoLog.Printf("Starting server on %s", *addr)
+  err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
+  errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+  db, err := sql.Open("mysql", dsn)
+  if err != nil {
+    return nil, err
+  }
+  if err = db.Ping(); err != nil {
+    return nil, err
+  }
+  return db, nil
+}
+
+type neuteredFileSystem struct {
+    fs http.FileSystem
+}
+
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+    f, err := nfs.fs.Open(path)
+    if err != nil {
+        return nil, err
+    }
+
+    s, err := f.Stat()
+    if s.IsDir() {
+        index := strings.TrimSuffix(path, "/") + "/index.html"
+        if _, err := nfs.fs.Open(index); err != nil {
+            return nil, err
+        }
+    }
+
+    return f, nil
+}
